17399efb97867b9125618ca2db4aba9f707da1cb
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-06-29 22:55:42 +0700

    updatehw01-3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -1,3 +1,19 @@
+
+/*
+    เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
+    
+    Test case:
+        First Name: Aomsin
+        Last Name: AomMoney
+    Output:
+        Aomsin AomMoneys TC, RMUTL, Chiang Mai, Thailand
+
+    Test case:
+        First Name: ABC
+        Last Name: DEF
+    Output:
+        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
+*/
 #include <stdio.h>
 
 int main() {
