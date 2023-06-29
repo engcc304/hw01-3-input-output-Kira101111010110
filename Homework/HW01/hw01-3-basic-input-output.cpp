5262c5dfc2e3ac2c98bf0628a37cf910ccee8a50
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-06-29 16:41:03 +0700

    updatehw1-3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -1,3 +1,17 @@
+#include <stdio.h>
+
+int main() {
+char FN[50]; 
+char LN[50];  
+printf("Test case:\n");
+    printf("    First Name: ");
+    scanf("%s", FN);
+    printf("    Last Name: ");
+    scanf("%s", LN);
+printf("Output:\n");
+    printf("    %s %s%s, %s, %s, %s\n", FN, LN, "s TC", "RMUTL", "Chiang Mai", "Thailand");
+return 0;
+}
 /*
     เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
     
