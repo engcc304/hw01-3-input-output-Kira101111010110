7dd827ccf90aa37f727522e7a145e8b9c3bb1138
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-06-29 22:49:45 +0700

    up datehw1-2

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -12,18 +12,3 @@ printf("Output:\n");
     printf("    %s %s%s, %s, %s, %s\n", FN, LN, "s TC", "RMUTL", "Chiang Mai", "Thailand");
 return 0;
 }
-/*
-    เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
-    
-    Test case:
-        First Name: Aomsin
-        Last Name: AomMoney
-    Output:
-        Aomsin AomMoneys TC, RMUTL, Chiang Mai, Thailand
-
-    Test case:
-        First Name: ABC
-        Last Name: DEF
-    Output:
-        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
-*/
